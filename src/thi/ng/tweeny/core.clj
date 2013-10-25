a9effef7861c621d5abacd7718647da63124dddf
Karsten Schmidt <k@postspectacular.com>
2013-10-25 19:29:56 +0100

    adding support for recursive tweening of maps & seqs and function wrapped values (for auto-animated tween targets), renaming `value-at` into `at`, splitting out value resolution into `resolve-value` & `tween-value` multi-method

--- src/thi/ng/tweeny/core.clj
+++ src/thi/ng/tweeny/core.clj
@@ -19,16 +19,16 @@
   ([x minIn maxIn minOut maxOut]
      (+ (* (- maxOut minOut) (/ (- x minIn) (- maxIn minIn))) minOut)))
 
-(defn mix
-  "Linear interpolation from a -> b at t."
-  [a b t] (+ (* (- b a) t) a))
-
 (defn smoothstep
   "S-Curve interpolation with curve between e0 e1.
   If x < e0 return 0.0, if x > e1 return 1.0"
   [e0 e1 x]
   (let [t (clamp (/ (- x e0) (- e1 e0)) 0.0 1.0)]
     (* t (* t (- 3.0 (* 2.0 t))))))
+
+(defn mix
+  "Linear interpolation from a -> b at t."
+  [a b t] (+ (* (- b a) t) a))
 
 (defn mix-linear
   "Returns linear interpolation fn."
@@ -55,32 +55,87 @@
   "Returns S-Curve interpolation fn for given edges (in range 0.0 .. 1.0)."
   [s e] (fn [a b t] (mix a b (smoothstep s e t))))
 
-(def kf
-  [[0.0 {:v [0 0 0] :f (mix-smoothstep 0.1 0.9)}]
-   [1.0 {:v [10 20 30] :f (mix-linear)}]
-   [2.0 {:v [100 10 200] :f (mix-circular false)}]
-   [4.0 {:v 100}]])
+(defn sinewave
+  [x phase freq amp]
+  (fn [t] (+ (* (Math/sin (+ (* t freq) phase)) amp) x)))
 
-(defn keyframes-for-t
+(defn- keyframes-at
   [t keyframes]
   (->> keyframes
        (partition 2 1)
        (drop-while (fn [[[t1] [t2]]] (not (in-range? t1 t2 t))))
        (first)))
 
-(defn val-at
-  [t keyframes]
-  (let [last-kf (peek keyframes)]
+(defn- resolve-value
+  [v t]
+  (cond
+   (number? v)     v
+   (fn? v)         (v t)
+   (map? v)        (reduce-kv
+                    (fn [acc k v]
+                      (if-not (number? v)
+                        (assoc acc k (resolve-value v t))
+                        acc))
+                    v v)
+   (sequential? v) (mapv
+                    (fn [v] (if-not (number? v) (resolve-value v t) v))
+                    v)
+   :default nil))
+
+(defmulti ^:private tween-value
+  (fn [v1 v2 f t]
     (cond
-     (neg? t) (-> keyframes first second :v)
-     (>= t (first last-kf)) (-> last-kf second :v)
+     (number? v1)     :num
+     (sequential? v1) :seq
+     (map? v1)        :map
+     :default         :default)))
+
+(defmethod tween-value :default
+  [v1 v2 f t] nil)
+
+(defmethod tween-value :num
+  [v1 v2 f t]
+  (cond
+   (number? v2)     (f v1 v2 t)
+   (sequential? v2) (mapv #(f v1 (resolve-value % t) t) v2)
+   :default         nil))
+
+(defmethod tween-value :seq
+  [v1 v2 f t]
+  (cond
+   (sequential? v2) (mapv
+                     #(tween-value
+                       (resolve-value %1 t)
+                       (resolve-value %2 t)
+                       f t)
+                     v1 v2)
+   (number? v2)     (mapv
+                     #(tween-value
+                       (resolve-value % t)
+                       v2 f t)
+                     v1)
+   :default nil))
+
+(defmethod tween-value :map
+  [v1 v2 f t]
+  (when (map? v2)
+    (reduce-kv
+     (fn [acc k v]
+       (if-let [v2* (v2 k)]
+         (assoc acc k (tween-value v v2* f t))
+         acc))
+     v1 v1)))
+
+(defn at
+  [t keyframes]
+  (let [kf1 (first keyframes) kf2 (peek keyframes)]
+    (cond
+     (<= t (first kf1)) (resolve-value (-> kf1 second :v) t)
+     (>= t (first kf2)) (resolve-value (-> kf2 second :v) t)
      :default
-     (let [[[t1 {v1 :v f :f}] [t2 {v2 :v}]] (keyframes-for-t t keyframes)
-           t (map-interval t t1 t2 0.0 1.0)
-           v2 (if (and (sequential? v1) (not (sequential? v2)))
-                (repeat (count v1) v2) v2)
-           v1 (if (and (sequential? v2) (not (sequential? v1)))
-                (repeat (count v2) v1) v1)]
-       (if (sequential? v1)
-         (mapv #(f %1 %2 t) v1 v2)
-         (f v1 v2 t))))))
+     (let [[[t1 {v1 :v f :f}] [t2 {v2 :v}]] (keyframes-at t keyframes)
+           t (map-interval t t1 t2 0.0 1.0)]
+       (tween-value
+        (resolve-value v1 t)
+        (resolve-value v2 t)
+        (or f mix) t)))))
